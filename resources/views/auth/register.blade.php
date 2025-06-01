0187d648b50d1542e52392819a3cea06b1d03202
NMarcell0001 <neme0001@hz.nl>
2025-06-01 15:32:18 +0200

    .

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" placeholder="yourname@example.com" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <p class="text-sm text-gray-500 mt-1">
                 Enter a valid email address. (Example: asd@gmail.com)
             </p><x-input-error :messages="$errors->get('email')" class="mt-2" />
