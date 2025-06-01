814b7feceebd1837bfa1e37d02ca18ecc3c35e19
NMarcell0001 <neme0001@hz.nl>
2025-06-01 14:44:34 +0200

    .

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,11 +1,9 @@
 @props(['messages'])
 
 @if ($messages)
-    <div {{ $attributes->merge(['class' => 'mt-2 p-3 border-l-4 border-orange-600 bg-white shadow-md rounded-md']) }}>
-        <ul class="list-none p-0 m-0 text-sm text-gray-800 space-y-1">
-            @foreach ((array) $messages as $message)
-                <li>{{ $message }}</li>
-            @endforeach
-        </ul>
-    </div>
+    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+        @foreach ((array) $messages as $message)
+            <li>{{ $message }}</li>
+        @endforeach
+    </ul>
 @endif
