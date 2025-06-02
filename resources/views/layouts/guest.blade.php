4fbe38dbdaa5388c2f048fa046eb5178516c8de1
NMarcell0001 <neme0001@hz.nl>
2025-06-02 09:01:28 +0200

    .

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,8 +11,18 @@
         <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
         <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
 
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @livewireStyles
+
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
