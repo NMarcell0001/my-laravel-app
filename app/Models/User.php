96e209020f8d2dc5e485531555015365a63adc26
NMarcell0001 <neme0001@hz.nl>
2025-06-11 21:19:47 +0200

    Potential fix again for the admin issue.

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     /**
@@ -39,5 +40,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'is_admin' => 'boolean',
     ];
 }
