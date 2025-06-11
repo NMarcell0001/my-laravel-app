687af29fbaef4ef5f87cb83fa79c8ea7121b4959
NMarcell0001 <neme0001@hz.nl>
2025-06-11 21:53:50 +0200

    Second admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,18 +3,38 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User; // Adjust if your User model is elsewhere
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     *
+     * This seeder creates two admin users:
+     * 1. 'asd@gmail.com'
+     * 2. 'anotheradmin@example.com'
+     *
+     * It uses `firstOrCreate` to prevent creating duplicate users if the seeder is run multiple times.
+     */
     public function run()
     {
-        User::create([
-            'name' => 'asd',
-            'email' => 'asd@gmail.com',
-            'password' => Hash::make('12345678'),
-            'is_admin' => true
-        ]);
+        User::firstOrCreate(
+            ['email' => 'asd@gmail.com'],
+            [
+                'name' => 'asd',
+                'password' => Hash::make('12345678'),
+                'is_admin' => true
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'anotheradmin@example.com'],
+            [
+                'name' => 'Another Admin',
+                'password' => Hash::make('securepassword'),
+                'is_admin' => true
+            ]
+        );
     }
 }
